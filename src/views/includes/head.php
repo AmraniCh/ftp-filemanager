8609396bdf056dce3902df46a6d03abeea65dbab
elamrani744 <elamrani.sv.laza@gmail.com>
2020-07-29 23:17:39 +0100

    Add required meta tags

--- src/views/includes/head.php
+++ src/views/includes/head.php
@@ -1,3 +1,8 @@
 <meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="description" content="A flexible FTP web application.">
+<meta name="keywords" content="ftp, filemanager, website">
+<link rel="author" content="El Amrani Chakir">
 <link rel="stylesheet" href="dist/style.min.css">
 <title><?= $title ?></title>

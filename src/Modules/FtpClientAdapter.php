de86aff6e1286ca4c9f8fdbb7c971a575d94b89d
elamrani744 <elamrani.sv.laza@gmail.com>
2020-08-02 02:14:35 +0100

    [FtpClientAdapter] fix

--- src/Modules/FtpClientAdapter.php
+++ src/Modules/FtpClientAdapter.php
@@ -1,17 +1,34 @@
 <?php
 
-
 namespace FTPApp\Modules;
 
+use Lazzard\FtpClient\Config\FtpConfig;
+use Lazzard\FtpClient\Connection\ConnectionInterface;
+use Lazzard\FtpClient\FtpClient;
 
 class FtpClientAdapter
 {
-    public function getFiles($directory)
+    /** @var ConnectionInterface */
+    public $connection;
+
+    /** @var FtpConfig */
+    public $config;
+
+    /** @var FtpClient */
+    public $client;
+
+    /**
+     * FtpClientAdapter constructor.
+     *
+     * @param ConnectionInterface $connection
+     * @param FtpConfig           $config
+     * @param FtpClient           $client
+     */
+    public function __construct(ConnectionInterface $connection, FtpConfig $config, FtpClient $client)
     {
-        return [
-            'file1',
-            'file2'
-        ];
+        $this->connection = $connection;
+        $this->config     = $config;
+        $this->client     = $client;
     }
 
     public function openConnection()

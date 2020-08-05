e34444d12a07de502108afec69104a8089d73ecc
elamrani744 <elamrani.sv.laza@gmail.com>
2020-08-05 13:55:22 +0100

    add docs

--- src/Modules/FtpAdapter.php
+++ src/Modules/FtpAdapter.php
@@ -4,7 +4,32 @@ namespace FTPApp\Modules;
 
 interface FtpAdapter
 {
-    public function openConnection();
+    /**
+     * Opens the FTP connection using the provided configs.
+     *
+     * @param array $config
+     *
+     * @throws FtpAdapterException
+     *
+     * @return void
+     */
+    public function openConnection($config);
+
+    /**
+     * Sets the passive mode on/off.
+     *
+     * @param bool $bool
+     *
+     * @return mixed
+     */
     public function setPassive($bool);
+
+    /**
+     * Gets a list of files in the giving directory.
+     *
+     * @param string $dir
+     *
+     * @return mixed
+     */
     public function browse($dir);
 }

67579d469e1acb735bd32562f4f1a9d4d96c3ae2
elamrani744 <elamrani.sv.laza@gmail.com>
2020-08-06 17:40:57 +0100

    Adding some helper methods

--- assets/js/entities/File.js
+++ assets/js/entities/File.js
@@ -3,12 +3,41 @@ var File = function (data) {
 
     this.name = data.name || '';
     this.type = data.type || 'file';
-    this.size = data.size || '';
+    this.size = parseInt(data.size) || 0;
     this.path = data.path || '';
-    this.owner = data.owner || '';
-    this.group = data.group || '';
+    this.owner = parseInt(data.owner) || '';
+    this.group = parseInt(data.group) || '';
     this.modifiedTime = data.modifiedTime || '';
     this.permissions = data.permissions || '';
+
+    File.prototype.bytesToSize = function () {
+        if (this.size === 0) return '0 Byte';
+
+        const sizes = {
+            Bytes: 1,
+            KB: Math.pow(1024, 1),
+            MB: Math.pow(1024, 2),
+            GB: Math.pow(1024, 3),
+            TB: Math.pow(1024, 4),
+        };
+
+        var unit = null;
+        for (var prop in sizes) {
+            if (this.size >= sizes[prop]) {
+                unit = prop;
+            }
+        }
+
+        return (this.size / sizes[unit]).toFixed(0) + ' ' + unit;
+    };
+
+    File.prototype.isFile = function () {
+        return this.type === 'file';
+    };
+
+    File.prototype.isDir = function () {
+            return this.type === 'dir';
+    };
 };
 
 export default File;

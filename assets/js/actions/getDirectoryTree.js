d341c8a94c967ba38d990bb9ed5f363e7518f038
elamrani744 <elamrani.sv.laza@gmail.com>
2020-08-12 01:55:15 +0100

    Fixing move file modal issue

--- assets/js/actions/getDirectoryTree.js
+++ assets/js/actions/getDirectoryTree.js
@@ -1,20 +1,21 @@
 import {fetchGet} from "../helpers/fetch";
 import DOMRender from "../helpers/DOMRender";
-import sidebarFileItem from "../templates/sidebarFileItem";
-import File from "../entities/File";
 import moveModalFileDirItem from "../templates/moveModalFileDirItem";
 import {getElement} from "../helpers/functions";
 
 function getDirectoryTree() {
     fetchGet('api?action=getDirectoryTree', function (res) {
         if (res.result) {
-
             getElement('.move-file-modal .files-list').textContent = '';
 
             res.result.forEach(function (dir) {
-                if (dir.path.indexOf('/') !== -1) {
+                // If the path starts with a slash remove it
+                dir.path = dir.path.replace(/^\//, '');
+
+                if (dir.path.indexOf('/') !== -1) { // Is a 'deep' path
                     const path = dir.path.slice(0, dir.path.lastIndexOf('/'));
-                    DOMRender(moveModalFileDirItem(dir), '.move-file-modal .files-list .dir-item[data-path="'+path+'"] .sub-files');
+                    // Append the content to the right dir item using the path
+                    DOMRender(moveModalFileDirItem(dir), `.move-file-modal .files-list .dir-item[data-path="${encodeURI(path)}"] .sub-files`);
                 } else {
                     DOMRender(moveModalFileDirItem(dir), '.move-file-modal .files-list');
                 }
@@ -22,7 +23,5 @@ function getDirectoryTree() {
         }
     });
 }
-
-
 
 export default getDirectoryTree;

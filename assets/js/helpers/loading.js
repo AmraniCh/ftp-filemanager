8f765418a6bd5a4e1379d5abd84771a111532494
elamrani744 <elamrani.sv.laza@gmail.com>
2020-08-17 06:21:26 +0100

    Fix loading

--- assets/js/helpers/loading.js
+++ assets/js/helpers/loading.js
@@ -1,8 +1,11 @@
+import {getElement} from "./functions";
+
 const
     sidebarLoader = fmWrapper.querySelector('.sidebar .loader'),
     tableLoader = fmWrapper.querySelector('.table-section .loader');
 
 function showLoading() {
+    getElement('.files-table tbody').textContent = '';
     fmWrapper.style.pointerEvents = 'none';
     sidebarLoader.classList.add('show');
     tableLoader.classList.add('show');

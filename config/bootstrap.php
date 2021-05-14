e83dcb5fb9fd29438302eba936466318ac2d6b82
AmraniCh <elamrani.sv.laza@gmail.com>
2021-05-14 18:49:57 +0000

    flip\whoops integration

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -1,6 +1,8 @@
 <?php
 
 use FTPApp\ErrorHandling\ErrorHandler;
+use Whoops\Run;
+use Whoops\Handler\PrettyPageHandler;
 
 require __DIR__ . '/../vendor/autoload.php';
 
@@ -9,48 +11,42 @@ ini_set('log_errors', true);
 ini_set('display_errors', true);
 ini_set('display_startup_errors', true);
 
-try {
-    $configFile = dirname(__DIR__) . '/config/app.php';
+/**
+ * ErrorHandler catch exceptions and set a custom error/exception
+ * handler according to if we're in a development or production mode,
+ * then a pretty page generated with whoops with a generic exception
+ * (production mode) or with exception details (dev mode).
+ */
+$whoops = new Run;
+$whoops->pushHandler(new PrettyPageHandler);
 
-    if (!file_exists($configFile)) {
-        throw new \RuntimeException("Application config file was missing.");
-    }
-
-    $config = include($configFile);
-
-    if ($config['debug'] === true) {
-        $handler = new ErrorHandler(function (\Exception $exx) {
-            echo "<h1>" . get_class($exx) . "</h1>";
-            echo "<h3>" . $exx->getMessage() . "</h3>";
-            echo "<p>File : " . $exx->getFile() . "</p>";
-            echo "<p>Line : " . $exx->getLine() . "</p>";
-            echo "<pre>Trace : " . $exx->getTraceAsString() . "</pre>";
-        });
-    }
-
-    if ($config['debug'] !== true) {
-        $handler = new ErrorHandler(function (\Exception $ex) {
-            // Build the message string
-            $message = sprintf(
-                "[%s] [%s] [%s] [Line : %s]\n%s\n",
-                date('Y:m:d h:m:s'),
-                $ex->getMessage(),
-                $ex->getFile(),
-                $ex->getLine(),
-                $ex->getTraceAsString()
-            );
-
-            // Logging error to a log file
-            error_log($message, 3, dirname(__DIR__) . '/storage/logs/'.date('Y-m-d').'.log');
-
-            // Send a friendly message to the user.
-            echo "<h3 style='color:#333;letter-spacing: 2px;'>Oops! Something goes wrong.</h3>";
-            exit();
-        });
-    }
-
-    $handler->start();
-
-} catch(\Exception $ex) {
-    exit($ex->getMessage());
+$configFile = dirname(__DIR__) . '/config/app.php';
+if (!file_exists($configFile)) {
+    $whoops->handleException(new \RuntimeException("Application config file is missing."));
 }
+
+$config = include($configFile);
+
+if ($config['debug']) {
+    $handler = new ErrorHandler(function (\Exception $ex) use($whoops) {
+        $whoops->handleException($ex);
+    });
+} elseif (!$config['debug']) {
+    $handler = new ErrorHandler(function (\Exception $ex) use($whoops) {
+        // Build the message string
+        $message = sprintf(
+            "[%s] [%s] [%s] [Line : %s]\n%s\n",
+            date('Y:m:d h:m:s'),
+            $ex->getMessage(),
+            $ex->getFile(),
+            $ex->getLine(),
+            $ex->getTraceAsString()
+        );
+        // logs the generated error info to a custom file
+        error_log($message, 3, dirname(__DIR__) . '/storage/logs/'.date('Y-m-d').'.log');
+        // send a friendly message to the user
+        $whoops->handleException(new Exception("Oops! Something goes wrong."));
+    });
+}
+
+$handler->start();
